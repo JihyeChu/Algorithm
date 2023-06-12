7a34912b3696f86953c22817f18de60f9fab3547
hojooglim <hojooglim@gmail.com>
2023-06-12 21:10:30 +0900

    문자열 정렬하기2

--- programmers/src/Prac.java
+++ programmers/src/Prac.java
@@ -1,30 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Prac {
     public static void main(String[] args) {
-        String my_string = "aBcDe";
-//        char[] answer = my_string.toCharArray();
-//        String trans = "";
-//        for (int i = 0; i < my_string.length(); i++) {
-//            int tmp = (int)answer[i];
-//            // 97 = a , 122 = z | 65 = A , 90 = Z
-//            if (97 <= tmp && tmp <= 122) {
-//                trans += (char)(tmp - 32);
-//            } else {
-//                trans += (char)(tmp + 32);
-//            }
-//        }
-//        System.out.println(trans);
+        String my_string = "aksjdnSDQ";
+        String a =my_string.toLowerCase();
+        ArrayList<Character> b =new ArrayList<>();
+
+        for (int i =0 ; i<a.length() ; i++){
+            char c = a.charAt(i);
+            b.add(c);
+        }
+
+        Collections.sort(b);
 
         String answer = "";
-        for(int i=0; i<my_string.length(); i++){
-            char trans = my_string.charAt(i);
-            if('a' <= trans && trans <= 'z') {
-                trans -= 32;
-            }else{
-                trans += 32;
-            }
-            answer += trans;
+
+        for(int i = 0; i < b.size(); i++)
+        {
+            answer += b.get(i);
         }
         System.out.println(answer);
+//        return answer;
     }
 
 }
