782b8ec016ada199b98926dba3c02b39a02b91bc
hojooglim <hojooglim@gmail.com>
2023-06-12 22:25:36 +0900

    문자열 정렬하기2

--- programmers/src/Prac.java
+++ programmers/src/Prac.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Prac {
     public static void main(String[] args) {
+        String my_string = "aksjdnSDQ";
+        String a =my_string.toLowerCase();
+        ArrayList<Character> b =new ArrayList<>();
 
+        for (int i =0 ; i<a.length() ; i++){
+            char c = a.charAt(i);
+            b.add(c);
+        }
+
+        Collections.sort(b);
+
+        String answer = "";
+
+        for(int i = 0; i < b.size(); i++)
+        {
+            answer += b.get(i);
+        }
+        System.out.println(answer);
+//        return answer;
     }
 
 }
