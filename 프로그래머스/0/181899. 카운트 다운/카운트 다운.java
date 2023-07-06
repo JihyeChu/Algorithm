271ac31413155416aac04f500b819d7ef54c4f9a
JihyeChu <09strawberry@naver.com>
2023-07-06 13:00:00 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] solution(int start, int end_num) {
         int[] answer = new int[start-end_num+1];
-        int num = 0;
-        for(int i=start; i>=end_num; i--){
-            answer[num++] = i;
+        for(int i=0; i<=start-end_num; i++){
+            answer[i] = start-i;
         }
         return answer;
     }
