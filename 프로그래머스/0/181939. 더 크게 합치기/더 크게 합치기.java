02a40ec6d73d72321b69a7aa59c0d98d1e498c4d
JihyeChu <09strawberry@naver.com>
2024-01-14 13:32:00 +0900

    [level 0] Title: 더 크게 합치기, Time: 7.81 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,13 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        int num1 = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
-        int num2 = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
-        if (num1 > num2 || num1 == num2) {
-            answer = num1;
-        }else{
-            answer = num2;
-        }
-        return answer;
+        int num1 = Integer.parseInt(""+a+b);
+        int num2 = Integer.parseInt(""+b+a);
+        return num1 < num2 ? num2 : num1;
     }
 }
