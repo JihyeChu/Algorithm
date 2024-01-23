ba2826497e8b3ae020445a19b23ca4d86397f48c
Jihye Chu <51440636+JihyeChu@users.noreply.github.com>
2024-01-23 19:48:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,14 +1,11 @@
 class Solution {
-   public int solution(int num) {
+  public int solution(int num) {
         int count = 0;
-        if(num == 1){ return 0;}
-        while(true){
+        while(num != 1){
             if(count >= 400){
                 return -1;
             }
-            if(num == 1){
-                break;
-            }else if(num%2 == 0){
+            if(num%2 == 0){
                 num = num/2;
             }else{
                 num = num*3+1;
