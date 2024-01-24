3ef10f561965552e060d2c8a2f98a30bbbb96c9a
Jihye Chu <51440636+JihyeChu@users.noreply.github.com>
2024-01-24 11:42:14 +0900

    [level 1] Title: 내적, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,10 +1,8 @@
 class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
-        int hap = 0;
         for(int i=0; i<a.length; i++){
-            hap = a[i]*b[i];
-            answer = answer+hap;
+            answer = answer + a[i]*b[i];
         }
 
         return answer;
