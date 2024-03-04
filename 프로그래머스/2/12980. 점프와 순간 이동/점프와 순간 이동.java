3786228618c76e8a2002586572280a53c2c1197d
JihyeChu <09strawberry@naver.com>
2024-03-04 16:44:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+     public int solution(int n) {
+        int answer = 0;
+        while(n >= 1) {
+            if(n % 2 == 0) {
+                n /= 2;
+                continue;
+            }
+            n--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
