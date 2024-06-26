3ec2a3a6776a9192055a16bf152197a6519fff63
JihyeChu <09strawberry@naver.com>
2024-06-26 21:01:06 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -2,22 +2,17 @@ class Solution {
     public String solution(String code) {
         String answer = "";
         int mode = 0;
-        
+
         for(int i=0; i<code.length(); i++){
             char currentChar = code.charAt(i);
-            
             if(currentChar == '1'){
-                mode = 1 - mode;    
-            }else{
-                if(mode == 0 && i%2 == 0){
-                    answer += currentChar;
-                }else if(mode == 1 && i%2 == 1){
-                    answer += currentChar;
-                }
+                mode = 1 - mode;
+                continue;
             }
-            
+            if(i%2 == mode){
+                answer += currentChar;
+            }
         }
-        
         return answer.length()==0?"EMPTY":answer;
     }
 }
