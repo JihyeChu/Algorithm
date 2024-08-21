8dae1bff5dc2f83e063e44cfe43ecf4155cf5106
JihyeChu <09strawberry@naver.com>
2024-08-21 20:43:42 +0900

    [level 0] Title: 수 조작하기 1, Time: 6.38 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -2,14 +2,12 @@ class Solution {
        public int solution(int n, String control) {
         int answer = n;
         for(char con : control.toCharArray()) {
-            if(con == 'w'){
-                answer += 1;
-            } else if (con == 's') {
-                answer -= 1;
-            } else if (con == 'd') {
-                answer += 10;
-            }else {
-                answer -= 10;
+            switch(con){
+                case 'w' : answer += 1; break;
+                case 's' : answer -= 1; break;
+                case 'd' : answer += 10; break;
+                case 'a' : answer -= 10; break;
+                default : break;
             }
         }
             return answer;
