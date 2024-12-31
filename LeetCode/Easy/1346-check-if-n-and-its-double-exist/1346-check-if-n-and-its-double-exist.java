997f16a4eeece5783096130827e4f4ac2fe995db
JihyeChu <09strawberry@naver.com>
2024-12-31 10:41:32 +0900

    Time: 2 ms (90.5%), Space: 43.2 MB (41.08%) - LeetHub

--- LeetCode/Easy/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ LeetCode/Easy/1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -2,13 +2,12 @@ import java.util.HashMap;
 
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        // 7, 1, 14, 11
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int a : arr){
             if(map.containsKey(a*2) || (a%2 == 0 && map.containsKey(a/2))){
                 return true;
             }
-            map.put(a, a*2); // 7, 14 | 1, 2 | 14, 28
+            map.put(a, a*2);
         }
         return false;
     }
