2e443b7a77d50652fe52436b70ae4208d8b8ef04
JihyeChu <09strawberry@naver.com>
2025-01-06 15:47:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ LeetCode/Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
         int len = arr.length;
-        
-        for(int i=0; i<len-1; i++){
-            int max = -1;
-            
-            for(int j=i+1; j<len; j++){
-                if(arr[j] > max){ 
-                    max = arr[j];
-                }
-            }
-            
+        int max = -1;
+
+        for(int i = len-1; i>=0 i--){
+            int currunt = arr[i];
             arr[i] = max;
+            max = math.Max(currunt, max);
         }
-        
-        arr[len-1] = -1;
+
         return arr;
     }
 }
