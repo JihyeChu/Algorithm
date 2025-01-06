a67029a30ced8d7b052836498036b33af323ebc7
JihyeChu <09strawberry@naver.com>
2025-01-06 15:46:50 +0900

    Time: 959 ms (6.36%), Space: 46.4 MB (76.68%) - LeetHub

---
+++ LeetCode/Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int len = arr.length;
+        
+        for(int i=0; i<len-1; i++){
+            int max = -1;
+            
+            for(int j=i+1; j<len; j++){
+                if(arr[j] > max){ 
+                    max = arr[j];
+                }
+            }
+            
+            arr[i] = max;
+        }
+        
+        arr[len-1] = -1;
+        return arr;
+    }
+}
