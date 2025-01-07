4b238279f969ebaf25caf689067d75f651a717ee
JihyeChu <09strawberry@naver.com>
2025-01-07 14:30:23 +0900

    Time: 2 ms (85.19%), Space: 46 MB (55.33%) - LeetHub

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.java
@@ -1,23 +1,16 @@
 class Solution {
-    static{
-        for(int i = 0 ;i<=500;i++){
-        moveZeroes(new int[]{});
-        }
-
-    }
-
-    public static void moveZeroes(int[] nums) {
-        int i=0;
-        int j=0;
-        int n = nums.length;
-        int temp=0;
-        for(;j<n;j++){
-            if(nums[j]!=0){
-                temp = nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                i+=1;
+    public void moveZeroes(int[] nums) {
+        int write = 0;
+        
+        for(int read=0; read<nums.length; read++){
+            if(nums[read] != 0){ 
+                nums[write++] = nums[read];
             }
         }
+        
+        for(int i=write; i<nums.length; i++){
+            nums[i] = 0;
+        }
+        
     }
 }
