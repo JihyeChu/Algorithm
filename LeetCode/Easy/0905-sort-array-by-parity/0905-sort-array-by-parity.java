1994f4e884033f28d852f99222bfe1132f590e3f
JihyeChu <09strawberry@naver.com>
2025-01-08 11:53:54 +0900

    Time: 1 ms (48.4%), Space: 45 MB (47.66%) - LeetHub

--- LeetCode/Easy/0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ LeetCode/Easy/0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -5,7 +5,6 @@ class Solution {
         int writeEven = 0;
         int writeOdd = nums.length-1;
         
-        // [3,1,2,4]
         for(int i=0; i<nums.length; i++) {
             if(nums[i]%2 == 0) {
                 result[writeEven++] = nums[i]; 
