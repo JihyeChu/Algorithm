72ec29c6877e76b49a70251501a7d306586464b2
JihyeChu <09strawberry@naver.com>
2025-03-06 21:00:00 +0000

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,12 +1,9 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2-num1+1];
-        int count = 0;
         
-        for(int i=num1; i<=num2; i++){
-            answer[count++] = numbers[i];
-        }    
+        return Arrays.copyOfRange(numbers, num1, num2+1);
             
-        return answer;
     }
 }
