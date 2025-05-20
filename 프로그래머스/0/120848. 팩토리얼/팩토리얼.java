ac0cb0858b43d47f9249a75a69e2646705bd75ca
JihyeChu <09strawberry@naver.com>
2025-05-20 10:01:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,13 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i=1; i<=10; i++){
-            if(n/i <= i){
-                answer = i;
-                break;
-            }
-            n /= i;
+        int answer = 1;
+        int factorial = 1;
+
+        while(n >= factorial) {
+            answer++;
+            factorial *= answer;
         }
-        return answer;
+        
+        return answer-1;
     }
 }
