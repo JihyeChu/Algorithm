75e65a309550ad0eaabd7fe03fa3406cb18037b1
JihyeChu <09strawberry@naver.com>
2025-07-31 19:34:00 +0900

    [level 0] Title: 외계행성의 나이, Time: 19.17 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        
+        while(age != 0) {
+            int num = age % 10;
+            answer = (char)(num + 'a') + answer;
+            age = age / 10;
+        }
+        
+        return answer;
+    }
+}
