3e37ae434d59f7ee1b35929f1e80951ad0d649b7
JihyeChu <09strawberry@naver.com>
2025-08-06 10:14:13 +0900

    [level 0] Title: 합성수 찾기, Time: 0.05 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,18 +2,15 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for(int i=4; i<=n; i++){
-            int count = 2;
-            for(int j=2; j<i; j++){
-                if(count >= 3){
+        for(int i = 4; i <= n; i++){
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
                     answer++;
                     break;
                 }
-                if(i%j == 0){
-                    count++;
-                }
             }
         }
+        
         return answer;
     }
 }
