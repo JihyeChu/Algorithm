f18fa6cb85aa15b44524362b9c91d733a601e1be
JihyeChu <09strawberry@naver.com>
2025-08-12 10:38:15 +0900

    [level 0] Title: 약수 구하기, Time: 2.47 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -10,10 +10,7 @@ class Solution {
             }
         }
         list.add(n);
-        int[] answer = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
-            answer[i] = list.get(i);
-        }
-        return answer;
+        
+        return list.stream().mapToInt(x -> x).toArray();
     }
 }
