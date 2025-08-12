2dca54988e3af57b9cda5a1b2c0c4bc2aa677275
JihyeChu <09strawberry@naver.com>
2025-08-12 10:02:40 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.17 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        int i = 0;
+        Map<String, String> map = new HashMap<>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+        
+        StringBuilder sb = new StringBuilder();
+        while(i < numbers.length()){
+            for(String key : map.keySet()){
+                if(numbers.startsWith(key, i)){
+                    sb.append(map.get(key));
+                    i += key.length();
+                    break;
+                }
+            }
+        }
+        
+        return Long.parseLong(sb.toString());
+    }
+}
