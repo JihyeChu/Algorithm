3e19cc24f610fbdde3e059cee12372341f766cb1
JihyeChu <09strawberry@naver.com>
2025-08-14 10:45:17 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.04 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,11 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i=1; i<=n/2; i++){
-            if(n == i*i){
-                return 1;
-            }
-        }
-        return 2;
+        
+        return Math.sqrt(n)%1==0 ? 1 : 2;
+        
     }
 }
