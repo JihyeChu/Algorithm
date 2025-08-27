69282eb686b86d504c5b9725d188c9f7b439b12a
JihyeChu <09strawberry@naver.com>
2025-08-28 00:04:18 +0900

    [level 0] Title: 등수 매기기, Time: 3.95 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,24 +1,29 @@
+import java.util.*;
+import javafx.util.Pair;
+
 class Solution {
     public int[] solution(int[][] score) {
-        double[] answer = new double[score.length];
         int[] result = new int[score.length];
-        int i = 0;
-        
-        
-        for(int[] s : score){
-            answer[i++] = (s[0]+s[1])/2.0;
+        List<Pair<Double, Integer>> list = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            double avg = (score[i][0] + score[i][1]) / 2.0;
+            list.add(new Pair<>(avg, i));
         }
-        
-        for(int j=0; j<answer.length; j++){
-            int rank = 1;
-            for(int k=0; k<answer.length; k++){
-                if(answer[j] < answer[k]){
-                    rank++;
-                }
+
+        list.sort((a, b) -> Double.compare(b.getKey(), a.getKey()));
+
+        int rank = 1;
+        result[list.get(0).getValue()] = rank;
+
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).getKey().equals(list.get(i - 1).getKey())) {
+                result[list.get(i).getValue()] = rank;
+            } else {
+                rank = i+1;
+                result[list.get(i).getValue()] = rank;
             }
-            result[j] = rank;
         }
-        
+
         return result;
     }
 }
