8dea7107766ef545dbe43196c631423e2de2b597
JihyeChu <09strawberry@naver.com>
2025-09-01 23:33:59 +0900

    [level 0] Title: 문자열 밀기, Time: 1.74 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,14 +1,6 @@
 class Solution {
     public int solution(String A, String B) {
-
-        if(A.equals(B)) return 0;
-
-        String temp = A;
-        for(int i=0; i<A.length(); i++){
-            temp = temp.charAt(temp.length()-1) + temp.substring(0, temp.length()-1);
-            if(temp.equals(B)) return i+1;
-        }
-        
-        return -1;
+        // B+B 이중에서 A 찾기
+        return (B+B).indexOf(A);
     }
 }
