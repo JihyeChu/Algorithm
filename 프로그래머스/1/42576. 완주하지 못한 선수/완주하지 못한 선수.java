0f1585726f4429cf1cf3fd73a65829c263626ee1
JihyeChu <09strawberry@naver.com>
2025-09-12 13:19:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 281.01 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,22 +2,16 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
-        }
-
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
-        }
-
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                return key;
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        
+        for(i=0; i<completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
             }
         }
-
-        return "";
+        return participant[i];
     }
 }
