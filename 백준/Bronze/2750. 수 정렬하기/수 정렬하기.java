90e33903a718e082e2f51415596336db972fdcc8
JihyeChu <09strawberry@naver.com>
2025-09-14 18:16:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,18 +1,23 @@
 import java.util.*;
+import java.io.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        Integer[] arr = new Integer[num];
-        
-        for(int i=0; i<num; i++){
-            arr[i] = sc.nextInt();
+class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        
         Arrays.sort(arr);
-        for(int a : arr){
-            System.out.println(a);
+
+        for(int i=0; i<n; i++){
+            sb.append(arr[i]).append("\n");
         }
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
